29b383c6f000229249b19299d18db2214e56502f
anubhab <anubhab_debnath@agnikul.in>
2024-03-13 13:00:51 +0530

    V0.0 added modifed trigger and server functions sync

---
+++ prototype_1/rough.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+#define BUFFER_SIZE 10
+
+
+// Struct to hold multiple arguments
+typedef struct {
+    int trig;
+    char message[BUFFER_SIZE];
+} thread_args;
+
+int fun_server(void *arguments)
+{
+    thread_args *args = (thread_args *)arguments;
+    while(1)
+    {
+        if(args->trig == 1)
+        {
+            // printf("var: %d\n", args->trig);
+            printf("sending message through server: %s\n", args->message);
+            args->trig = 0;
+        }
+    }
+}
+
+int fun_trigger(void *arguments)
+{
+    thread_args *args = (thread_args *)arguments;
+    while(1)
+    {
+        printf("enter message: \n");
+        fgets(args->message,BUFFER_SIZE,stdin);
+        args->trig = 1;
+    }
+}
+
+// init process 
+int main()
+{
+    pthread_t thread_id1, thread_id2;
+
+    thread_args *arg_1 = malloc(sizeof(thread_args));
+
+    if (arg_1 == NULL) 
+    {
+        perror("Failed to allocate memory for arguments\n");
+        exit(EXIT_FAILURE);
+    }
+
+    arg_1->trig = 0;
+    pthread_create(&thread_id1,NULL, (void *)fun_server, arg_1);
+    pthread_create(&thread_id2,NULL, (void *)fun_trigger, arg_1);
+
+    pthread_exit(NULL);
+}
